e39c9bfc2a39c17dfb33c62c2447d585fec3508f
Tommy Chen <tommy351@gmail.com>
2020-06-02 00:01:54 +0800

    Update gallery tab

--- lib/screens/home/gallery_tab.dart
+++ lib/screens/home/gallery_tab.dart
@@ -4,6 +4,8 @@ import 'package:ehreader/screens/gallery/args.dart';
 import 'package:ehreader/screens/gallery/screen.dart';
 import 'package:ehreader/stores/gallery.dart';
 import 'package:ehreader/widgets/center_progress_indicator.dart';
+import 'package:ehreader/widgets/rating_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
@@ -75,7 +77,7 @@ class _GalleryTabContentState extends State<_GalleryTabContent> {
 
             return _buildRow(context, galleries.elementAt(i));
           },
-          separatorBuilder: (context, i) => Divider(),
+          separatorBuilder: (context, i) => Divider(height: 1),
           itemCount:
               pagination.noMore ? galleries.length : galleries.length + 1,
         );
@@ -84,23 +86,64 @@ class _GalleryTabContentState extends State<_GalleryTabContent> {
   }
 
   Widget _buildRow(BuildContext context, Gallery gallery) {
-    return ListTile(
-      key: Key('${gallery.id.id}'),
-      title: Text(gallery.title),
-      subtitle: Text(gallery.titleJpn),
-      contentPadding: EdgeInsets.all(0),
-      leading: CachedNetworkImage(
-        imageUrl: gallery.thumbnail,
-        width: 80,
-        fit: BoxFit.cover,
+    final ThemeData theme = Theme.of(context);
+    final thumbSize = 92.0;
+
+    return InkWell(
+      child: Row(
+        key: Key('${gallery.id.id}'),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          CachedNetworkImage(
+            imageUrl: gallery.thumbnail,
+            width: thumbSize,
+            height: thumbSize,
+            fit: BoxFit.cover,
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 8),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            '${gallery.category} / ${gallery.fileCount}P',
+                            style: theme.textTheme.caption,
+                          ),
+                        ),
+                        Text(
+                          '${gallery.rating}',
+                          style: theme.textTheme.caption,
+                        ),
+                        RatingBar(
+                          gallery.rating,
+                          size: 16,
+                          color: theme.primaryColor,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    gallery.title,
+                    style: theme.textTheme.subtitle1,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
       onTap: () {
-        Navigator.pushNamed(
-          context,
-          GalleryScreen.routeName,
-          arguments:
-              GalleryScreenArguments((b) => b..id = gallery.id.toBuilder()),
-        );
+        Navigator.pushNamed(context, GalleryScreen.routeName,
+            arguments:
+                GalleryScreenArguments((b) => b..id = gallery.id.toBuilder()));
       },
     );
   }
---
+++ lib/widgets/rating_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class RatingBar extends StatelessWidget {
+  final double rating;
+  final double size;
+  final Color color;
+
+  const RatingBar(
+    this.rating, {
+    Key key,
+    this.size,
+    this.color,
+  })  : assert(rating != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: Iterable.generate(5)
+          .map((e) => Icon(
+                _getIconData(rating - e),
+                color: color,
+                size: size,
+              ))
+          .toList(),
+    );
+  }
+
+  IconData _getIconData(double n) {
+    if (n >= 0.75) return Icons.star;
+    if (n >= 0.3) return Icons.star_half;
+    return Icons.star_border;
+  }
+}
